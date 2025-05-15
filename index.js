d1b1d6129b2130ee63567f0bf3259c6a555e2932
Luis Fernando Sánchez Agostini <lfsagostini@gmail.com>
2025-05-15 14:05:59 -0400

    movement player success

--- index.js
+++ index.js
@@ -2,10 +2,13 @@
 // S - start
 // C - Candy
 // G - Ghost
+// ? - enigm
+// P - player
 const start = 'Start';
 const candy = 'Candy';
 const ghost = 'Ghost';
 const enigm = '?';
+const player = 'Player';
 
 const mansion = [
   [start, enigm, enigm, enigm],
@@ -30,7 +33,20 @@ const listEnigms = [
   },
 ];
 
-const startGame = () => {
+const rlInterface = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const prompt = (question) => {
+  return new Promise((resolve) => {
+    rlInterface.question(question, (answer) => {
+      resolve(answer);
+    });
+  });
+};
+
+const startGame = async () => {
   console.info('Starting the game...');
   console.table(mansion);
 
@@ -40,7 +56,56 @@ const startGame = () => {
   };
   let candyFound = false;
 
-  while (!candyFound) {}
+  while (!candyFound) {
+    // Get the player's move
+    // using WASD
+    // W = up, A = left, S = down, D = right
+    const move = await prompt(
+      'Enter your move (W - up, S - down, A- left, D- right): '
+    );
+
+    console.info(`You moved ${move}`);
+    // Update the player's position
+    switch (move.toUpperCase()) {
+      case 'W':
+        if (playerTarget.x > 0) playerTarget.x--;
+        break;
+      case 'S':
+        if (playerTarget.x < mansion.length - 1) playerTarget.x++;
+        break;
+      case 'A':
+        if (playerTarget.y > 0) playerTarget.y--;
+        break;
+      case 'D':
+        if (playerTarget.y < mansion[0].length - 1) playerTarget.y++;
+        break;
+      default:
+        console.info('Invalid move');
+    }
+
+    // Check player position slot
+    const currentSlot = mansion[playerTarget.x][playerTarget.y];
+
+    if (currentSlot === candy) {
+      console.info('You found the candy! You win!');
+      candyFound = true;
+    } else if (currentSlot === ghost) {
+      console.info('You found the ghost! You need to answer two enigms!');
+      // Ask two enigms
+      break;
+    } else if (currentSlot === enigm) {
+      // Ask an enigm
+      const enigm = listEnigms[Math.floor(Math.random() * listEnigms.length)];
+      const answer = await prompt(`${enigm.question} `);
+      if (answer.toLowerCase() === enigm.response.toLowerCase()) {
+        console.info('Good job! You answered correctly.');
+        mansion[playerTarget.x][playerTarget.y] = player;
+      } else {
+        console.info('Wrong answer! Try again.');
+      }
+    }
+    console.table(mansion);
+  }
 };
 
 startGame();
